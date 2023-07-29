d147567e4c50d136c30195ba9d65e44f3e1c32ba
Szymon Polaczy <thomas_frey@tutanota.com>
2023-07-29 10:59:59 +0200

    Added tailwind - styled the login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,27 +5,45 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>School Journal</title>
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ url('/css/dist.css') }}" />
 </head>
 
-<body class="antialiased">
-    <form method="POST" action="{{ route('login') }}">
+<body class="antialiased grid md:grid-cols-2 justify-center items-center
+    min-h-screen py-16 px-4 md:px-16 gap-4 md:gap-12"
+>
+    <img src="{{ url('images/undraw_educator_re_ju47.svg') }}" alt=""
+        class="row-start-2 md:row-start-1"
+    />
+
+    <form method="POST" action="{{ route('login') }}"
+        class="flex align-middle justify-center flex-col gap-4 max-w-sm shadow-gray-300
+               shadow-2xl mx-auto rounded-lg p-8"
+    >
         @csrf
+
+        <h1 class="text-2xl text-gray-900 text-center">
+            Login
+        </h1>
+
         <div class="form-group mb-3">
-            <input type="text" placeholder="Email" id="email" 
-                class="form-control" name="email" required autofocus>
+            <input type="text" placeholder="Email" id="email"
+                class="form-control border-gray-300 border-b-2 p-1
+                hover:border-gray-500 transition-all"
+                name="email" required autofocus>
             @if ($errors->has('email'))
             <span class="text-danger">{{ $errors->first('email') }}</span>
             @endif
         </div>
 
         <div class="form-group mb-3">
-            <input type="password" placeholder="Password" id="password" 
-                class="form-control" name="password" required>
+            <input type="password" placeholder="Password" id="password"
+                class="form-control border-gray-300 border-b-2 p-1
+                hover:border-gray-500 transition-all"
+                name="password" required>
             @if ($errors->has('password'))
             <span class="text-danger">{{ $errors->first('password') }}</span>
             @endif
@@ -40,7 +58,11 @@
         </div>
 
         <div class="d-grid mx-auto">
-            <button type="submit" class="btn btn-dark btn-block">Signin</button>
+            <button type="submit" class="d-block rounded-md bg-blue-500
+                    text-white text-lg p-2 hover:bg-blue-700 transition-all"
+            >
+                Signin
+            </button>
         </div>
     </form>
 </body>
