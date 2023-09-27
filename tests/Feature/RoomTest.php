a79438f1d1b3580597457296d6d726023101c6ca
Szymon Polaczy <thomas_frey@tutanota.com>
2023-09-27 16:27:37 +0200

    Checking not only response and errors but also name change

--- tests/Feature/RoomTest.php
+++ tests/Feature/RoomTest.php
@@ -98,17 +98,26 @@ class RoomTest extends TestCase
     public function test_editing_a_class_not_logged_in() {
         $new_name = $this->generateRandomString(10);
         $room_id = collect(Room::first())->first();
+        $room = Room::find($room_id);
+        $og_name = $room->name;
 
         $response = $this->put('/edit-room/' . $room_id, [
             'name' => $new_name,
         ]);
 
         $response->assertStatus(403);
+
+        $room = Room::find($room_id);
+        $new_name = $room->name;
+
+        $this->assertSame($og_name, $new_name);
     }
 
     public function test_editing_a_class_logged_in_not_admin() {
         $new_name = $this->generateRandomString(10);
         $room_id = collect(Room::first())->first();
+        $room = Room::find($room_id);
+        $og_name = $room->name;
 
         $users = User::role('teacher')->get();
         Auth::login($users[0]);
@@ -129,11 +138,18 @@ class RoomTest extends TestCase
         ]);
 
         $response->assertStatus(403);
+
+        $room = Room::find($room_id);
+        $new_name = $room->name;
+
+        $this->assertSame($og_name, $new_name);
     }
 
     public function test_editing_a_class_logged_in() {
         $new_name = $this->generateRandomString(10);
         $room_id = collect(Room::first())->first();
+        $room = Room::find($room_id);
+        $og_name = $room->name;
 
         $users = User::role('admin')->get();
         Auth::login($users[0]);
@@ -143,11 +159,18 @@ class RoomTest extends TestCase
         ]);
 
         $response->assertStatus(302);
+
+        $room = Room::find($room_id);
+        $new_name = $room->name;
+
+        $this->assertNotSame($og_name, $new_name);
     }
 
     public function test_editing_a_class_logged_in_too_long_name() {
         $new_name = $this->generateRandomString(300);
         $room_id = collect(Room::first())->first();
+        $room = Room::find($room_id);
+        $og_name = $room->name;
 
         $users = User::role('admin')->get();
         Auth::login($users[0]);
@@ -158,6 +181,11 @@ class RoomTest extends TestCase
 
         $response->assertSessionHasErrors(['name']);
         $response->assertStatus(302);
+
+        $room = Room::find($room_id);
+        $new_name = $room->name;
+
+        $this->assertSame($og_name, $new_name);
     }
 
 
